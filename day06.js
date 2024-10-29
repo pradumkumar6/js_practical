16edc05463f5977175b4dc88084aa32a41f44e93
Pradum Kumar <pradumjaiswar790@gmail.com>
2024-10-29 06:04:31 +0530

    updated file

---
+++ day06.js
@@ -0,0 +1,34 @@
+// let score = "99";
+
+// important
+// let score = "99abc";
+
+// console.log(typeof score);
+
+// let number = Number(score);
+// console.log(typeof number);
+// console.log(number);
+
+// "33" => number
+// "33abc" => NaN
+// true => 1 , false =>0
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = 0;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let isLoggedIn = "pradum";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0=> false
+// "" => false
+// "pradum" => true
---
+++ main.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let email = "pradumkumar0612@gmail.com";
+const city = "Jaunpur";
+let password = "22ms0095";
+
+// let state;
+
+// Prefer not to use var because of issue in block scope and functional scope
+
+console.table([accountId, email, city, password]);
+// if we only create a variable and not assign any value then js code show the undefined in output
+
+// console.table([accountId, email, city, password, state]);
+
+console.log(typeof undefined);
